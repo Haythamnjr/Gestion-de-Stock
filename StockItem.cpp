14c7e8bd3caa51347d38e0db6f814e24a4ecf624
Haythamnjr <Haytham.nejjari@gmail.com>
2026-09-22 23:12:40 +0200

    Implement database update and insert for StockItem
    Added database interaction methods for updating and inserting stock items, including error handling for SQL exceptions.

--- StockItem.cpp
+++ StockItem.cpp
@@ -1,4 +1,5 @@
 #include "StockItem.h"
+#include "Date.h"
 #include<iostream>
 #include "Exception.h"
 #include<cmath>
@@ -12,6 +13,7 @@ StockItem::StockItem(int Day , int Month , int Year ,int quantite,int id,string 
 int StockItem::getId()const {
 	return this->id;
 }
+
 void StockItem::setId(int id) {
 	if (id < 0) {
 		throw Exception("Entrer un nombre positif!");
@@ -70,6 +72,23 @@ void StockItem::updateStock(string NomProduit,int newQuantity, double newPrice) 
 	this->NomProduit = NomProduit;
 	this->quantite = newQuantity;
 	this->unitPrice = newPrice;
+	try {
+		unique_ptr<sql::PreparedStatement>stmtUp(
+			data->prepareStatement("UPDATE Stock SET Quantite = ?,NomProduit = ?,unitPrice = ? WHERE id = ?")
+		);
+
+		stmtUp->setInt(1, newQuantity);
+		stmtUp->setString(2, NomProduit);
+		stmtUp->setDouble(3, newPrice);
+		stmtUp->setInt(4, this->getId());
+		stmtUp->executeUpdate();
+
+	}
+	catch (sql::SQLException& e) {
+		cerr << "Le mise a jour est echec :" << e.what() << endl;
+		cerr << "SQLState" << e.getSQLState() << endl;
+		cerr << "Code Erreur" << e.getErrorCode() << endl;
+	}
 
 
 }
@@ -77,6 +96,27 @@ void StockItem::updateStock(string NomProduit,int newQuantity, double newPrice) 
 double StockItem::updateCalculeValue()const {
 	return this->calculateTotalValue();
 }
+
+bool StockItem::DataStock()const {
+	try {
+		unique_ptr<sql::PreparedStatement>stmt(
+			data->prepareStatement("INSERT INTO Stock(id,Quantite,NomProduit,unitPrice) VALUES(?,?,?,?)")
+		);
+		stmt->setInt(1, this->getId());
+		stmt->setInt(2, this->getQuantite());
+		stmt->setString(3, this->getNomProduit());
+		stmt->setDouble(4, this->getunite());
+		stmt->execute();
+		cout << "Les donnes est Enregistre" << endl;
+		return true;
+
+	}
+	catch (sql::SQLException& e) {
+		cerr << "Probleme :" << e.what() << endl;
+		return false;
+	}
+}
+
 string StockItem::Afficher() const{
 	return format("id de cette Produit est :{} |le nom de produit est :{} | la quantite de cette produit :{} | le prix de cette produit est :{} DH | La date est : {}/{}/{}" , this->id ,this->NomProduit , this->quantite , this->unitPrice,this->getDay(),this->getMonth(),this->getYear());
 }
